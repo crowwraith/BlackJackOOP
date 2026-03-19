6520ff26544b724de09c130e1fd6a9ea48ee4088
crowwraith <1185615@student.roc-nijmegen.nl>
2026-03-19 11:41:21 +0100

    player number buttons
    added player number buttons to choose number of players,
    
    minor style changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,51 +30,176 @@
         {
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            flowLayoutPanel1.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.BackColor = Color.FromArgb(255, 128, 0);
-            button1.FlatAppearance.BorderColor = Color.Sienna;
-            button1.FlatAppearance.BorderSize = 2;
+            button1.BackColor = Color.WhiteSmoke;
+            button1.FlatAppearance.BorderColor = Color.Red;
+            button1.FlatAppearance.BorderSize = 3;
             button1.FlatStyle = FlatStyle.Flat;
             button1.ForeColor = SystemColors.ActiveCaptionText;
-            button1.Location = new Point(319, 293);
+            button1.Location = new Point(3, 3);
             button1.Name = "button1";
             button1.Size = new Size(167, 34);
             button1.TabIndex = 0;
             button1.Text = "Spel starten";
             button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            button1.Click += Gamestart_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Location = new Point(289, 44);
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label1.ForeColor = Color.Cornsilk;
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(246, 25);
+            label1.Size = new Size(430, 43);
             label1.TabIndex = 1;
             label1.Text = "hoeveel spelers doen er mee?";
-            label1.Visible = false;
+            label1.ClientSizeChanged += label1_ClientSizeChanged;
+            label1.Click += None;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.WhiteSmoke;
+            button2.FlatAppearance.BorderColor = Color.Red;
+            button2.FlatAppearance.BorderSize = 3;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(357, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 2;
+            button2.Tag = "4";
+            button2.Text = "4 spelers";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += playerbutton_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.WhiteSmoke;
+            button3.FlatAppearance.BorderColor = Color.Red;
+            button3.FlatAppearance.BorderSize = 3;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(239, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 3;
+            button3.Tag = "3";
+            button3.Text = "3 spelers";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += playerbutton_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.WhiteSmoke;
+            button4.FlatAppearance.BorderColor = Color.Red;
+            button4.FlatAppearance.BorderSize = 3;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(121, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 4;
+            button4.Tag = "2";
+            button4.Text = "2 spelers";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += playerbutton_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.WhiteSmoke;
+            button5.FlatAppearance.BorderColor = Color.Red;
+            button5.FlatAppearance.BorderSize = 3;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(3, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 34);
+            button5.TabIndex = 5;
+            button5.Tag = "1";
+            button5.Text = "1 speler";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += playerbutton_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Anchor = AnchorStyles.None;
+            flowLayoutPanel1.BackColor = Color.Transparent;
+            flowLayoutPanel1.Controls.Add(button5);
+            flowLayoutPanel1.Controls.Add(button4);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Location = new Point(621, 354);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(490, 56);
+            flowLayoutPanel1.TabIndex = 6;
+            flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Anchor = AnchorStyles.None;
+            flowLayoutPanel2.BackColor = Color.Transparent;
+            flowLayoutPanel2.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanel2.Controls.Add(label1);
+            flowLayoutPanel2.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            flowLayoutPanel2.ForeColor = Color.Moccasin;
+            flowLayoutPanel2.Location = new Point(718, 171);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(489, 56);
+            flowLayoutPanel2.TabIndex = 7;
+            flowLayoutPanel2.Paint += flowLayoutPanel2_Paint;
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.Anchor = AnchorStyles.None;
+            flowLayoutPanel3.BackColor = Color.Transparent;
+            flowLayoutPanel3.Controls.Add(button1);
+            flowLayoutPanel3.Location = new Point(801, 732);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(171, 54);
+            flowLayoutPanel3.TabIndex = 8;
+            flowLayoutPanel3.Paint += flowLayoutPanel3_Paint;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Aquamarine;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Controls.Add(button1);
+            BackColor = Color.DarkOliveGreen;
+            ClientSize = new Size(1738, 845);
+            Controls.Add(flowLayoutPanel3);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(flowLayoutPanel1);
             Name = "Form1";
             Text = " ";
+            Load += Form1_Load;
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel2.PerformLayout();
+            flowLayoutPanel3.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanel3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,15 +10,64 @@ namespace BlackJackOOP
 
             InitializeComponent();
             Card card1 = new Card(Rank.KING, Suit.HEARTS, false);
-            
+
             Deck deck = new Deck();
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Gamestart_Click(object sender, EventArgs e)
         {
-            
+
         }
+        private void playerbutton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int nummer = Convert.ToInt32(btn.Tag);
+
+            MessageBox.Show("Je klikte op knop " + nummer);
+        }
+
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            flowLayoutPanel1.Left = (this.ClientSize.Width - flowLayoutPanel1.Width) / 2;
+            flowLayoutPanel1.Top = (this.ClientSize.Height - flowLayoutPanel1.Height) / 2;
+        }
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+            flowLayoutPanel2.Left = (this.ClientSize.Width - flowLayoutPanel2.Width) / 2;
+            flowLayoutPanel2.Top = 30;
+        }
+        private void flowLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+            flowLayoutPanel3.Left = (this.ClientSize.Width - flowLayoutPanel3.Width) / 2;
+            int marge = (int)(this.ClientSize.Height * 0.20); // 20% marge vanaf de button zelf.
+            flowLayoutPanel1.Top = this.ClientSize.Height - flowLayoutPanel1.Height - marge;
+        }
+
+
+
+        private void label1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label1.Top = 10;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label1.Top = 10;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label1.Top = 10;
+        }
+
+        private void None(object sender, EventArgs e)
+        {
+
+        }
+
 
     }
 }
