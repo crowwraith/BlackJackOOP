06555a40e559d461e6a5ba5383ee3cf464bee563
crowwraith <1185615@student.roc-nijmegen.nl>
2026-03-25 12:20:44 +0100

    hands and players
    hand seems to be functional, player needs a bit more work to propperly work
    
    card pictures removed

--- Form1.cs
+++ Form1.cs
@@ -1,73 +1,120 @@
 using BlackJackOOP.Enums;
 using BlackJackOOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
 
 namespace BlackJackOOP
 {
     public partial class Form1 : Form
     {
+        private List<Player> players = new List<Player>();        // nooit null
+        private Deck deck = new Deck();                            // direct een nieuw deck
+        private List<FlowLayoutPanel> panels = new List<FlowLayoutPanel>();
+
         public Form1()
         {
-
             InitializeComponent();
-            Card card1 = new Card(Rank.KING, Suit.HEARTS, false);
-
-            Deck deck = new Deck();
-
         }
 
-        private void Gamestart_Click(object sender, EventArgs e)
-        {
-
-        }
+        // Event handler voor "Aantal spelers" buttons
         private void playerbutton_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            int nummer = Convert.ToInt32(btn.Tag);
+            int aantalSpelers = Convert.ToInt32(btn.Tag);
 
-            MessageBox.Show("Je klikte op knop " + nummer);
+            // Maak nieuw deck
+            deck = new Deck();
+
+            // Maak spelers en bijbehorende panelen
+            players = new List<Player>();
+            panels = new List<FlowLayoutPanel>();
+
+            // verwijder oude dynamische panelen (als die er waren)
+            foreach (var pnl in panels)
+            {
+                this.Controls.Remove(pnl);
+                pnl.Dispose();
+            }
+            panels.Clear();
+
+            for (int i = 0; i < aantalSpelers; i++)
+            {
+                // speler aanmaken
+                players.Add(new Player());
+
+                // dynamisch panel voor deze speler
+                FlowLayoutPanel panel = new FlowLayoutPanel();
+                panel.Width = 400;
+                panel.Height = 130;
+                panel.Top = 20 + i * 150; // verticale spacing
+                panel.Left = 20;
+                panel.BackColor = Color.LightGray;
+                panel.BorderStyle = BorderStyle.FixedSingle;
+
+                this.Controls.Add(panel);
+                panels.Add(panel);
+
+                // geef 2 kaarten aan speler
+                Hand hand = players[i].Hands[0];
+                hand.AddCard(deck.DrawCard());
+                hand.AddCard(deck.DrawCard());
+
+                // toon kaarten in panel
+                foreach (var card in hand.Cards)
+                {
+                    ShowCard(card, panel);
+                }
+            }
         }
 
-
-        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        // Event handler voor testknop "Gamestart"
+        private void Gamestart_Click(object sender, EventArgs e)
         {
-            flowLayoutPanel1.Left = (this.ClientSize.Width - flowLayoutPanel1.Width) / 2;
-            flowLayoutPanel1.Top = (this.ClientSize.Height - flowLayoutPanel1.Height) / 2;
+            if (players == null || players.Count == 0)
+            {
+                MessageBox.Show("Selecteer eerst het aantal spelers!");
+                return;
+            }
+
+            // Voor test: toon alle spelers en hun kaarten in MessageBox
+            string info = "";
+            for (int i = 0; i < players.Count; i++)
+            {
+                info += $"Speler {i + 1}: ";
+                foreach (var card in players[i].Hands[0].Cards)
+                {
+                    info += card.ToString() + ", ";
+                }
+                info += Environment.NewLine;
+            }
+            MessageBox.Show(info);
         }
-        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+
+        // Toon een kaart in een FlowLayoutPanel
+        private void ShowCard(Card card, FlowLayoutPanel panel)
         {
-            flowLayoutPanel2.Left = (this.ClientSize.Width - flowLayoutPanel2.Width) / 2;
-            flowLayoutPanel2.Top = 30;
+            PictureBox pb = new PictureBox();
+            pb.Width = 80;
+            pb.Height = 120;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // Path naar kaart image
+            string path = Path.Combine(Application.StartupPath, "PNG-cards-1.3", card.GetImageFileName());
+
+            if (File.Exists(path))
+            {
+                pb.Image = Image.FromFile(path);
+            }
+            else
+            {
+                // fallback: lege kleur als bestand niet gevonden
+                pb.BackColor = Color.Green;
+            }
+
+            panel.Controls.Add(pb);
         }
-        private void flowLayoutPanel3_Paint(object sender, PaintEventArgs e)
-        {
-            flowLayoutPanel3.Left = (this.ClientSize.Width - flowLayoutPanel3.Width) / 2;
-            int marge = (int)(this.ClientSize.Height * 0.20); // 20% marge vanaf de button zelf.
-            flowLayoutPanel1.Top = this.ClientSize.Height - flowLayoutPanel1.Height - marge;
-        }
-
-
-
-        private void label1_ClientSizeChanged(object sender, EventArgs e)
-        {
-            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
-            label1.Top = 10;
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
-            label1.Top = 10;
-        }
-        private void Form1_Resize(object sender, EventArgs e)
-        {
-            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
-            label1.Top = 10;
-        }
-
-        private void None(object sender, EventArgs e)
-        {
-
-        }
-
-
     }
 }
--- Models/Deck.cs
+++ Models/Deck.cs
@@ -5,21 +5,50 @@ using System.Text;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 using BlackJackOOP.Models;
 namespace BlackJackOOP.Models;
+
 public class Deck
 {
-    public List<Card> cards { get; private set; }
+    public List<Card> Cards { get; private set; }
 
     public Deck()
-
     {
-        cards = new List<Card>();
+        Cards = new List<Card>();
 
         foreach (Rank rank in Enum.GetValues<Rank>())
         {
             foreach (Suit suit in Enum.GetValues<Suit>())
             {
-                cards.Add(new Card(rank, suit, false));
+                Cards.Add(new Card(rank, suit, false));
             }
         }
+
+        Shuffle();
+    }
+
+    public void Shuffle()
+    {
+        Random rng = new Random();
+
+        int n = Cards.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+
+            Card temp = Cards[k];
+            Cards[k] = Cards[n];
+            Cards[n] = temp;
+        }
+    }
+
+    public Card DrawCard()
+    {
+        if (Cards.Count == 0)
+            throw new InvalidOperationException("Deck is leeg!");
+
+        Card card = Cards[0];
+        Cards.RemoveAt(0);
+
+        return card;
     }
 }
